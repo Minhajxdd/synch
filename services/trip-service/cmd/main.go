256a43cf41c146480839b8e991b91505de37e504
Mohammed Minhaj <nijulinux@gmail.com>
2025-09-27 16:55:23 +0530

    refact: refactored modules

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"github.com/Minhajxdd/Synch/services/trip-service/internal/infrastructure/repository"
+	"github.com/Minhajxdd/Synch/services/trip-service/internal/service"
+)
+
+func main() {
+	inMemRepo := repository.NewInmemRepository()
+	_ = service.NewService(inMemRepo)
+
+	for {
+		time.Sleep(time.Second)
+	}
 }
